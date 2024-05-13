54665d9f6caf01f1027dab81467bb3c89c6d1ab1
Takahiro Arai <231205005@ccmailg.meijo-u.ac.jp>
2024-05-13 16:54:04 +0900

    テトリミノの色をすべて青色に変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,11 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-<<<<<<< Updated upstream
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
-=======
     'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue'
->>>>>>> Stashed changes
 ];
 
 // creates a new 4x4 shape in global variable 'current'
